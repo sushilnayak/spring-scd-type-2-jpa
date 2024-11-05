750c6a621ba8681b1c5f8825071493ce6d2a2c4e
Sushil Nayak <sushil.nayak@gmail.com>
2024-11-05 20:04:54 +0530

    initial

--- src/main/java/com/nayak/springscdtype2jpa/model/Employee.java
+++ src/main/java/com/nayak/springscdtype2jpa/model/Employee.java
@@ -8,6 +8,7 @@ import lombok.Setter;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -33,4 +34,17 @@ public class Employee extends SCDType2Entity {
         return Set.of("employeeId");
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(getId(), employee.getId()) &&
+                Objects.equals(employeeId, employee.employeeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), employeeId);
+    }
 }
